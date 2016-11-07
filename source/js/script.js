d25c293fee30cdbb7ddb21b0abb46bb8d5c377c8
Igor Gulyaev <Igor Gulyaev>
2016-11-07 02:20:19 +0200

    Front fixes 4

--- source/js/script.js
+++ source/js/script.js
@@ -9,13 +9,13 @@ function mapToggleFn() {
         if (listToggle.classList.contains('active')) {
             bodyBlock.classList.remove("map-on");
             mapToggle.classList.remove("active");
-            jQuery('#map').toggle('slide', 'left', 500);
+            //jQuery('#map').toggle('slide', 'left', 500);
         }
     } else {
         if (listToggle.classList.contains('active')) {
             bodyBlock.classList.add("map-on");
             mapToggle.classList.add("active");
-            jQuery('#map').toggle('slide', 'left', 500);
+            //jQuery('#map').toggle('slide', 'left', 500);
         }
     }
 }
@@ -24,13 +24,13 @@ function listToggleFn() {
         if (mapToggle.classList.contains('active')) {
             bodyBlock.classList.remove("list-on");
             listToggle.classList.remove("active");
-            jQuery('#demo').toggle('slide', 'left', 500);
+            //jQuery('#demo').toggle('slide', 'left', 500);
         }
     } else {
         if (mapToggle.classList.contains('active')) {
             bodyBlock.classList.add("list-on");
             listToggle.classList.add("active");
-            jQuery('#demo').toggle('slide', 'left', 500);
+            //jQuery('#demo').toggle('slide', 'left', 500);
         }
     }
 }
